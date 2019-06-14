8e86e9bfa917c593f145e5422bd4a4eaf6b8e255
kain <zhangtqx@vip.qq.com>
2019-06-14 22:00:04 +0800

    submit

--- database/migrations/2019_06_14_210729_create_router_table.php
+++ database/migrations/2019_06_14_210729_create_router_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAdminTable extends Migration
+class CreateRouterTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,19 +13,18 @@ class CreateAdminTable extends Migration
      */
     public function up()
     {
-        Schema::create('admin', function (Blueprint $table) {
+        Schema::create('router', function (Blueprint $table) {
             $table->increments('id')->comment('主键');
-            $table->string('username', 20)->unique()->comment('用户名');
-            $table->text('password')->comment('口令');
-            $table->string('call', 10)->nullable()->comment('称呼');
-            $table->string('phone', 11)->nullable()->comment('手机号');
-            $table->string('email', 50)->nullable()->comment('电子邮件');
-            $table->string('avatar', 100)->nullable()->comment('头像');
+            $table->unsignedInteger('parent')->default(0)->comment('父级关联');
+            $table->json('name')->comment('路由名称');
+            $table->boolean('nav')->unsigned()->default(0)->comment('是否为路由');
+            $table->string('icon', 30)->nullable()->comment('字体图标');
+            $table->string('routerlink', 90)->nullable()->comment('路由地址');
+            $table->unsignedTinyInteger('sort')->default(0)->comment('排序');
             $table->boolean('status')->unsigned()->default(1)->comment('状态');
             $table->unsignedInteger('create_time')->default(0)->comment('创建时间');
             $table->unsignedInteger('update_time')->default(0)->comment('更新时间');
         });
-
     }
 
     /**
@@ -35,6 +34,6 @@ class CreateAdminTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('admin');
+        Schema::dropIfExists('router');
     }
 }
--- database/migrations/2019_06_14_210738_create_role_table.php
+++ database/migrations/2019_06_14_210738_create_role_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAdminTable extends Migration
+class CreateRoleTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,19 +13,16 @@ class CreateAdminTable extends Migration
      */
     public function up()
     {
-        Schema::create('admin', function (Blueprint $table) {
+        Schema::create('role', function (Blueprint $table) {
             $table->increments('id')->comment('主键');
-            $table->string('username', 20)->unique()->comment('用户名');
-            $table->text('password')->comment('口令');
-            $table->string('call', 10)->nullable()->comment('称呼');
-            $table->string('phone', 11)->nullable()->comment('手机号');
-            $table->string('email', 50)->nullable()->comment('电子邮件');
-            $table->string('avatar', 100)->nullable()->comment('头像');
+            $table->json('name')->comment('权限组名称');
+            $table->binary('router')->comment('路由授权');
+            $table->binary('api')->comment('接口授权');
+            $table->text('note')->nullable()->comment('备注');
             $table->boolean('status')->unsigned()->default(1)->comment('状态');
             $table->unsignedInteger('create_time')->default(0)->comment('创建时间');
             $table->unsignedInteger('update_time')->default(0)->comment('更新时间');
         });
-
     }
 
     /**
@@ -35,6 +32,6 @@ class CreateAdminTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('admin');
+        Schema::dropIfExists('role');
     }
 }
