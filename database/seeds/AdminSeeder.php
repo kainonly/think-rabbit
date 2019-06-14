f60ac9dd58d87caa339604da353a85f6ec94bb44
kain <zhangtqx@vip.qq.com>
2019-06-15 00:33:11 +0800

    submit database

--- database/seeds/AdminSeeder.php
+++ database/seeds/AdminSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class AdminSeeder extends Seeder
 {
@@ -11,6 +13,11 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('admin')->insert([
+            'username' => 'admin',
+            'password' => Hash::make('12345678'),
+            'create_time' => time(),
+            'update_time' => time()
+        ]);
     }
 }
