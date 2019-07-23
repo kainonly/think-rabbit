642cd3c593c3999ff817345485d52cc6cde08316
kain <zhangtqx@vip.qq.com>
2019-07-23 09:17:01 +0800

    Submit

--- config/cors.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'supportsCredentials' => false,
-    'allowedOrigins' => explode(',', env('ALLOWED_ORIGINS')),
-    'allowedHeaders' => ['Content-Type', 'X-Requested-With'],
-    'allowedMethods' => ['POST'],
-    'exposedHeaders' => [],
-    'maxAge' => 31536000,
-];
