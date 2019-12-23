2c8a1b5637d69a1c8fd4cf0b57f0755e3300f80e
kain <zhangtqx@vip.qq.com>
2019-12-23 11:11:19 +0800

    完善配置

--- src/service/AMQPService.php
+++ src/service/AMQPService.php
@@ -4,8 +4,8 @@ declare (strict_types=1);
 
 namespace think\amqp\service;
 
+use simplify\amqp\AMQPClient;
 use think\Service;
-use tidy\amqp\RabbitClient;
 
 final class AMQPService extends Service
 {
@@ -14,8 +14,6 @@ final class AMQPService extends Service
         $this->app->bind('amqp', function () {
             $config = $this->app->config
                 ->get('queue.rabbitmq');
-
-            return new RabbitClient($config);
         });
     }
 }
